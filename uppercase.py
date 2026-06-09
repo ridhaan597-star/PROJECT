684af432085024fbc25812db03a163abe6e8bb73
ridhaan597-star <ridhaan597@gmail.com>
2026-06-09 21:10:03 +0530

    Add files via upload

---
+++ uppercase.py
@@ -0,0 +1,10 @@
+a = "congratulations"
+print(a.upper())
+print("MORE EXAMPELS")
+b = "happybirthday"
+print(b.upper())
+c = "WHATSUP"
+print(c.lower())
+print("MORE EXAMPELS")
+d = "GOODNIGHT"
+print(d.lower())
