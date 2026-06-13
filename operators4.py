2882a0e759b1a4f1aa58d8d8959815ea08112287
ridhaan597-star <ridhaan597@gmail.com>
2026-06-13 12:59:30 +0530

    Add files via upload

---
+++ operators4.py
@@ -0,0 +1,30 @@
+a = 10 
+b = 12 
+c = 0
+if a and b and c:
+    print("All the numbers have boolean valu as true")
+else:
+    print("atleast 1 numbers hass boolean value as false ")
+
+a = 10
+b = -12 
+c = 0
+if a>0 or b>0 or c>0:
+    print("All the numbers have boolean valu as true")
+else:
+    print("atleast 1 numbers hass boolean value as false ")
+  
+a = 10 
+b = 12 
+c = 12 
+print(not(a == b))
+print(not(b == c))
+a = "pythonnn"
+b = "coding"
+if not (a == b ):
+    print(a, 'and', b, 'are different.')
+a = 4
+b = 5
+if not ((a == 1) == (b == 5)):
+    print("hello")
+    
