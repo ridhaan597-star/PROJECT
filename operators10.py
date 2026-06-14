efadd337d67139b91360934fad06f29f4bef48d8
ridhaan597-star <ridhaan597@gmail.com>
2026-06-14 12:45:18 +0530

    Add files via upload

---
+++ operators10.py
@@ -0,0 +1,12 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = (v+w)*x/y
+print(z)
+name = "Ridhaan"
+age = 13
+if name == "Ridhaan" or name =="john" and age >= 2:
+    print("HI")
+else:
+    print("bye")
---
+++ operators11.py
@@ -0,0 +1,7 @@
+numn =int(input("enter a numerator: "))
+numd =int(input("enter a denomerator: "))
+if numn % numd == 0:
+    print("true")
+else:
+    print("false")
+    
