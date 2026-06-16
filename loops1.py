420e8d307af05002a53ce4523a0bd6755f0e8501
ridhaan597-star <ridhaan597@gmail.com>
2026-06-16 12:44:55 +0530

    Add files via upload

---
+++ loops1.py
@@ -0,0 +1,16 @@
+n = int(input("enter a number: "))
+for n in range(n,0,-1):
+    print(n)
+
+n = int(input("enter a number: "))
+sum=0
+for i in range(1,n+1):
+    sum=i+sum
+    print(sum)
+ 
+string =(input("enter a sentence: "))
+string2=("")
+for i in string:
+    string2=i+string2
+print(string)    
+print(string2)   
