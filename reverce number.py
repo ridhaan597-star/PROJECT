bea660ae43204b555278d6bda2ee6cc0e78400de
ridhaan597-star <ridhaan597@gmail.com>
2026-06-20 14:01:43 +0530

    Add files via upload

---
+++ reverce number.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+
+num = abs(num)
+
+if num == 0:
+    count = 1
+else:
+    count = 0
+    while num > 0:
+        num = num // 10
+        count += 1
+
+print("Number of digits:", count)
