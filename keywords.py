28da8bd08218c179bb178fb7bca6058ab8e28d87
ridhaan597-star <ridhaan597@gmail.com>
2026-06-23 13:06:52 +0530

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,33 @@
+print("Q1)",end=" ")
+a = input("enter a word: ")
+for i in (a):
+    if i=='A':
+        print("'A' is found ")
+        break
+    else:
+        print(i,"found","'A' is not found")
+print()       
+var = int(input("enter a number: "))
+while var>0:
+    var=var-1
+    if var==5:
+        continue 
+    else:
+        print(var)
+print("Gooooood Byeeeeeeeeee")
+print()
+for x in range(0,10):
+    if x % 20 == 0:
+        print ("twist")
+    elif x % 15 == 0:
+        pass
+    elif x % 5 == 0:
+        print ("fizz")
+    elif x % 3 == 0:
+        print ("buzz")
+    else:
+        print(x)
+
+    
+    
+    
