15302caa829c88bf0b2ca64b8233ae4bcbaec420
ridhaan597-star <ridhaan597@gmail.com>
2026-07-01 17:13:04 +0530

    Add files via upload

---
+++ enter  yopur age.py
@@ -0,0 +1,13 @@
+age = int(input("Enter your age: "))
+
+if age > 10 and age < 20:
+    print("Entry allowed")
+else:
+    print("Entry not allowed")
+if age%2 == 0:
+    print("even")
+
+else:
+    print("odd")
+if age==float:
+    print("enter a proper digit")
